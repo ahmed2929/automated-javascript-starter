ab043cb23dc6ec4fc3a1e8971d62843c6eed53ae
AK <mywork304@gmail.com>
2020-06-14 21:33:51 +0200

    adding babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,7 @@
-const express=require('express');
-const open =require('open')
-const path=require('path')
+import express from 'express';
+import open from 'open';
+import path from 'path';
+
 const app=express();
 const port=3000
 app.get('/',(req,res)=>{
---
+++ buildScripts/startMassage.js
@@ -0,0 +1,2 @@
+import chalk from 'chalk'
+console.log(chalk.green('app started in dev mode'))
