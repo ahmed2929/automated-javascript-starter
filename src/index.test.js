829c9dcfeb93010d03f220edec6d498b2e36c598
AK <mywork304@gmail.com>
2020-06-19 21:20:37 +0200

    f

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,7 @@
 import {expect} from 'chai'
-import jsdom from 'mocha-jsdom'
-import fs from 'fs'
 describe('our first test',()=>{
     it('shoud pass',()=>{
-        expect(true).to.equal(true)
+        expect(true).to.equal(false)
     })
 
 })
